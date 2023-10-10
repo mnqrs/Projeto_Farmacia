782ac91ae63efea0063bbbeb5b3ee740f1899fa8
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-10-10 10:19:30 -0300

    Criação banco de dados

--- Projeto_Final/Data/AppDbContext.cs
+++ Projeto_Final/Data/AppDbContext.cs
@@ -4,7 +4,7 @@ namespace Projeto_Final.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
     }
